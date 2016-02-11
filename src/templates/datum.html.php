5d0eb03f0dc48b910bcbe93dba54d10a9247ee40
Alex Grintsvayg <alex@grin.io>
2016-02-11 14:39:34 -0500

    move raw/json links above content

--- src/templates/datum.html.php
+++ src/templates/datum.html.php
@@ -1,5 +1,5 @@
-<pre id="datum"><?php echo htmlspecialchars($datum->content) ?></pre>
 <div style="text-align: right;"><a href="?raw">raw</a> | <a href="?json">json</a></div>
+<pre id="datum"><?php echo htmlspecialchars($datum->content) ?></pre>
 <script src="/js/lzw.js"></script>
 <script src="/js/sjcl.js"></script>
 <script src="/js/txt.js"></script>
